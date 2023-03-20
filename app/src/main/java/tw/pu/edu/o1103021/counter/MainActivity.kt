4212f3a70980da5d1bb51ce90f2b369334f03e06
Jason <o1103021@gm.pu.edu.tw>
2023-03-20 09:55:00 +0800

    第一次Commit

---
+++ app/src/main/java/tw/pu/edu/o1103021/counter/MainActivity.kt
@@ -0,0 +1,11 @@
+package tw.pu.edu.o1103021.counter
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
